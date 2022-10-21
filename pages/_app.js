c97e3080713454ba3e5550f36d0f4405aa508a78
Lukas Dobbles <computorgenius101@gmail.com>
2022-10-20 17:29:31 -0700

    basic virtual assistant :)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "regenerator-runtime/runtime";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/generate.js
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from "openai";
+import execute from "../../lib/functions";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const response = await openai.createCompletion({
+    model: "davinci",
+    prompt: generateRequest(req.body.request),
+    temperature: 0.6,
+    max_tokens: 200,
+    stop: ["\n"],
+    frequency_penalty: 2,
+    presence_penalty: 0,
+  });
+
+  let result = response.data.choices[0].text;
+  try {
+    result = JSON.parse(result);
+    res.status(200).send(await execute(result));
+  } catch (error) {
+    console.log("result", result);
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
+const generateRequest = (
+  request
+) => `This is a virtual assistant named Frank that classifies requests and outputs what should be executed in JSON form. There are three ID's with different functions: speak, time, and weather. Frank loves to have conversations.
+why is the sky blue-->{"id":"speak","arguments":["Sunlight reaches Earth's atmosphere and is scattered in all directions by all the gases and particles in the air."]}
+how many houseflies are there?-->{"id":"speak","arguments":["I don't know."]}
+what time is it-->{"id":"time"}
+who is the president-->{"id":"speak","arguments":["Joseph R. Biden"]}
+what is the weather-->{"id":"weather"}
+open YouTube-->{"id":"code","arguments":["window.open('youtube.com')"]}
+what does the word obstreperous mean-->{"id":"word","arguments":["obstreperous"]}
+${request}-->`;
