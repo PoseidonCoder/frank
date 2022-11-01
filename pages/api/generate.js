5d31d3223cfd393edf2a0befc3053d35b86fbfee
Lukas Dobbles <computorgenius101@gmail.com>
2022-11-01 13:09:18 -0700

    fixed weather

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,9 +31,9 @@ const generateRequest = (
 ) => `This is a virtual assistant named Frank that classifies requests and outputs what should be executed in JSON form. There are four ID's with different functions: speak, time, weather, and dictionary (to look up words).
 why is the sky blue-->{"id":"speak","arguments":["Sunlight reaches Earth's atmosphere and is scattered in all directions by all the gases and particles in the air."]}
 how many houseflies are there?-->{"id":"speak","arguments":["I don't know."]}
-what time is it-->{"id":"time"}
+what time is it-->{"id":"time","arguments":[]}
 who is the president-->{"id":"speak","arguments":["Joseph R. Biden"]}
-what is the weather-->{"id":"weather"}
+what is the weather-->{"id":"weather","arguments":[]}
 open YouTube-->{"id":"code","arguments":["window.open('youtube.com')"]}
 what does the word obstreperous mean-->{"id":"dictionary","arguments":["obstreperous"]}
 what is the meaning of paradox-->{"id":"dictionary","arguments":["paradox"]}
